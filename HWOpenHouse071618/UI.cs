c2dc4bf3606347dcc3264de26cf1342958f77c43
Chip <chip.elmer@gmail.com>
2018-08-20 18:34:26 -0500

    Added UI class and refactored to use

---
+++ HWOpenHouse071618/UI.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HWOpenHouse071618
+{
+    static class UI
+    {
+        public static bool UserAnswersYes(string question)
+        {
+            // TODO: improve this method and make it cleaner
+
+            Console.WriteLine(question);
+
+            string response = Console.ReadLine().ToUpper();
+
+            if (response == "Y" || response == "YES")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
